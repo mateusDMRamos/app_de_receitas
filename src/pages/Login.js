7cbaf0c4dbfabf825995538835ae88e8ab3acd95
Mateus de Medeiros Ramos <mateusdmramos@gmail.com>
2022-11-29 16:27:18 -0300

    criada validação do botão de login e cobertura de 45% dos testes da página de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function Login() {
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [btnDisabled, setDisabled] = useState(true);
+
+  useEffect(() => {
+    const validateLogin = () => {
+      const minLength = 6;
+      const defaultEmail = /^([a-zA-Z0-9_\-.]+)@([a-zA-Z0-9_\-.]+)\.([a-zA-Z]{2,5})$/;
+      if (defaultEmail.test(email) && password.length > minLength) {
+        setDisabled(false);
+      } else {
+        setDisabled(true);
+      }
+    };
+    validateLogin();
+  }, [password, email]);
+
+  const handleChange = (value, setValue) => {
+    setValue(value);
+  };
+
   return (
     <form>
       <h2>Login</h2>
@@ -8,15 +29,19 @@ function Login() {
         type="email"
         data-testid="email-input"
         placeholder="Email"
+        onChange={ ({ target }) => handleChange(target.value, setEmail) }
       />
       <input
         type="password"
         data-testid="password-input"
         placeholder="Password"
+        onChange={ ({ target }) => handleChange(target.value, setPassword) }
       />
+      <br />
       <button
         type="button"
         data-testid="login-submit-btn"
+        disabled={ btnDisabled }
       >
         Enter
       </button>
