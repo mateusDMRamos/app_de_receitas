aebc9ff148ac4f5d01c1f070f19d237953270138
João <joaolelles7@gmail.com>
2022-12-01 14:35:12 -0300

    Requisito 57, Feat: Implementando testId

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -8,6 +8,31 @@ function Profile() {
       <header>
         <Header title="Profile" searchIcon={ false } />
       </header>
+      <body>
+        <p
+          data-testid="profile-email"
+        >
+          Email
+        </p>
+        <button
+          type="button"
+          data-testid="profile-done-btn"
+        >
+          Done Recipes
+        </button>
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+        >
+          Favorite Recipes
+        </button>
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+        >
+          Logout
+        </button>
+      </body>
       <Footer />
     </>
   );
