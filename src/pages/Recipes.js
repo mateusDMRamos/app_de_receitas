8c91dc8023bab4bc09dfdbb03d915d5709889e8f
Mateus de Medeiros Ramos <mateusdmramos@gmail.com>
2022-12-01 16:10:26 -0300

    requisito 14 - feat: agora os 12 primeiros resultados das pesquisas são renderizados na página de receitas

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,27 +1,41 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import recipesContext from '../context/recipesContext';
 import Footer from '../components/Footer';
+import RecipeCard from '../components/RecipeCard';
 
 function Recipes({ history }) {
-  const { setHistory, recipes } = useContext(recipesContext);
+  const { setHistory, recipes, setRecipes } = useContext(recipesContext);
+  const [firstRecipes, setFirstRecipes] = useState([]);
+
   useEffect(() => { setHistory(history.location.pathname); });
   useEffect(() => {
     if (recipes.drinks) {
-      console.log(recipes.drinks);
       if (recipes.drinks.length === 1) {
         history.push(`/drinks/${recipes.drinks[0].idDrink}`);
+        setRecipes({ notSearched: true });
       }
+      const arraySize = 11;
+      const firstRecipesFound = recipes.drinks.filter((
+        (recipe, index) => index <= arraySize
+      ));
+      setFirstRecipes(firstRecipesFound);
     } else if (recipes.meals) {
-      console.log(recipes.meals);
       if (recipes.meals.length === 1) {
         history.push(`/meals/${recipes.meals[0].idMeal}`);
+        setRecipes({ notSearched: true });
       }
+      const arraySize = 11;
+      const firstRecipesFound = recipes.meals.filter((
+        (recipe, index) => index <= arraySize
+      ));
+      setFirstRecipes(firstRecipesFound);
     } else if (!recipes.notSearched) {
       global.alert('Sorry, we haven\'t found any recipes for these filters.');
     }
-  }, [recipes, history]);
+  }, [recipes, history, setRecipes]);
+
   return (
     <>
       <header>
@@ -31,6 +45,29 @@ function Recipes({ history }) {
             : <Header title="Drinks" searchImage />
         }
       </header>
+      {firstRecipes.map((recipe, index) => {
+        if (recipe.idDrink) {
+          return (
+            <RecipeCard
+              index={ index }
+              data-testid={ `${index}-recipe-card` }
+              key={ recipe.idDrink }
+              recipeId={ recipe.idDrink }
+              recipeName={ recipe.strDrink }
+              thumb={ recipe.strDrinkThumb }
+            />
+          );
+        }
+        return (
+          <RecipeCard
+            index={ index }
+            key={ recipe.idMeal }
+            recipeId={ recipe.idMeal }
+            recipeName={ recipe.strMeal }
+            thumb={ recipe.strMealThumb }
+          />
+        );
+      })}
       <Footer />
     </>
   );
