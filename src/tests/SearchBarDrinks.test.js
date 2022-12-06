10fae24ed89a62b9774b1d9c0f5e675ac3079350
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-12-06 18:07:21 -0300

    Aumentando a cobertura de testes da aplicação

--- src/tests/SearchBarDrinks.test.js
+++ src/tests/SearchBarDrinks.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import { screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { act } from 'react-dom/test-utils';
 import renderWithRouter from './helpers';
 import App from '../App';
+import { allDrinks } from './mocks/drinksFetchMocks';
+import { allMeals } from './mocks/mealsFetchMocks';
+import mealsCategoriesMocks from './mocks/mealsCategoriesMock';
+import drinksCategoriesMocks from './mocks/drinksCategoriesMock';
 
 const TEST_EMAIL = 'teste@email.com';
 const PASSWORD = '1234567';
@@ -13,18 +17,39 @@ const inputBtn = 'login-submit-btn';
 const submitButtonSearch = 'exec-search-btn';
 const searchIcon = 'search-top-btn';
 const searcInputHeader = 'search-input';
+const ingredientRadioBtn = 'ingredient-search-radio';
 
-describe('Testa o componente searchBar na Página Drinks', () => {
+const allMealsFetch = () => Promise.resolve({
+  json: () => Promise.resolve(allMeals),
+});
+const allDrinksFetch = () => Promise.resolve({
+  json: () => Promise.resolve(allDrinks),
+});
+const mealCategoriesFetch = () => Promise.resolve({
+  json: () => Promise.resolve(mealsCategoriesMocks),
+});
+const drinkCategoriesFetch = () => Promise.resolve({
+  json: () => Promise.resolve(drinksCategoriesMocks),
+});
+
+describe('Testa o componente SearchBar na tela Drinks', () => {
   beforeEach(() => {
-    global.fetch = jest.fn(() => Promise.resolve({
-      json: () => Promise.resolve({}),
-    }));
+    global.fetch = jest.fn()
+      .mockImplementationOnce(allDrinksFetch)
+      .mockImplementationOnce(drinkCategoriesFetch);
   });
 
   afterEach(() => {
     global.fetch.mockClear();
   });
+
   it('Verifica se os elementos são renderizados', async () => {
+    global.fetch = jest.fn()
+      .mockImplementationOnce(allMealsFetch)
+      .mockImplementationOnce(mealCategoriesFetch)
+      .mockImplementationOnce(allDrinksFetch)
+      .mockImplementationOnce(drinkCategoriesFetch);
+
     const { history } = renderWithRouter(<App />);
     const inputEmailLogin = screen.getByTestId(inputEmail);
     const inputPasswordLogin = screen.getByTestId(inputPassword);
@@ -35,7 +60,7 @@ describe('Testa o componente searchBar na Página Drinks', () => {
     userEvent.click(inputBtnLogin);
 
     const mealsTitle = await screen.getByTestId('page-title');
-    const ingredientRadio = await screen.getByTestId('ingredient-search-radio');
+    const ingredientRadio = await screen.getByTestId(ingredientRadioBtn);
     const nameRadio = await screen.getByTestId('name-search-radio');
     const firstLetterRadio = await screen.getByTestId('first-letter-search-radio');
     const searchButton = await screen.getByTestId(submitButtonSearch);
@@ -57,7 +82,7 @@ describe('Testa o componente searchBar na Página Drinks', () => {
       history.push('/drinks');
     });
 
-    const ingredientRadio = screen.getByTestId('ingredient-search-radio');
+    const ingredientRadio = screen.getByTestId(ingredientRadioBtn);
     const searchIconHeader = screen.getByTestId(searchIcon);
 
     userEvent.click(searchIconHeader);
@@ -120,10 +145,9 @@ describe('Testa o componente searchBar na Página Drinks', () => {
     userEvent.click(searchButton);
     expect(global.alert).toHaveBeenCalledWith('Your search must have only 1 (one) character');
   });
-  it('Verifica se os radioButton name funciona', () => {
-    global.alert = jest.fn(() => {});
-
+  it('Verifica se os radioButton name funciona', async () => {
     const { history } = renderWithRouter(<App />);
+    global.alert = jest.fn(() => {});
 
     act(() => {
       history.push('/drinks');
@@ -135,10 +159,14 @@ describe('Testa o componente searchBar na Página Drinks', () => {
 
     const searchInputHeader = screen.getByTestId(searcInputHeader);
 
-    userEvent.type(searchInputHeader, 'rice');
+    userEvent.type(searchInputHeader, 'Feijão');
+
+    const ingredientRadio = screen.getByTestId(ingredientRadioBtn);
+
+    userEvent.click(ingredientRadio);
 
     const searchButton = screen.getByTestId(submitButtonSearch);
     userEvent.click(searchButton);
-    expect(global.alert).toHaveBeenCalledWith('Sorry, we haven\'t found any recipes for these filters.');
+    await waitFor(() => expect(global.alert).toHaveBeenCalledWith('Sorry, we haven\'t found any recipes for these filters.'));
   });
 });
