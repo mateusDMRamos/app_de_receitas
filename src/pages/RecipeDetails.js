315ed56c7cfdb81f7fb9c371c36f223cee6349b4
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-12-07 15:19:59 -0300

    requisito 24, implementando API de detalhes

--- src/pages/RecipeDetails.js
+++ src/pages/RecipeDetails.js
@@ -1,12 +1,24 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { fetchMealsDetails } from '../services/meals';
+import { fetchDrinksDetails } from '../services/drinks';
 
 function RecipeDetails({ history, match: { params: { id } } }) {
+  const [details, setDetails] = useState('');
+  console.log(details);
   useEffect(() => {
-    const results = fetchMealsDetails(id);
-    console.log(results);
-  }, [id, results]);
+    const { pathname } = history.location;
+    const results = async () => {
+      if (pathname.includes('meals')) {
+        const responseMealsDetails = await fetchMealsDetails(id);
+        setDetails(responseMealsDetails.meals[0]);
+      } else {
+        const responseDrinksDetails = await fetchDrinksDetails(id);
+        setDetails(responseDrinksDetails.drinks[0]);
+      }
+    };
+    results();
+  }, [history.location, id]);
   return (
     <header>
       <h1>Recipe details</h1>
@@ -18,6 +30,11 @@ RecipeDetails.propTypes = {
   match: PropTypes.shape({
     params: PropTypes.shape({
       id: PropTypes.string.isRequired,
+    }),
+  }).isRequired,
+  history: PropTypes.shape({
+    location: PropTypes.shape({
+      pathname: PropTypes.string,
     }),
   }).isRequired,
 };
