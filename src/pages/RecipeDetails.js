03fd48be0dcf856ef0bb584362389e2935681a79
Carla Beatriz Rabelo <carlabeatrizrabelo@gmail.com>
2022-12-07 14:44:24 -0300

    Avançando no Requisito 24

--- src/pages/RecipeDetails.js
+++ src/pages/RecipeDetails.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { fetchMealsDetails } from '../services/meals';
 
-function RecipeDetails() {
+function RecipeDetails({ history, match: { params: { id } } }) {
+  useEffect(() => {
+    const results = fetchMealsDetails(id);
+    console.log(results);
+  }, [id, results]);
   return (
     <header>
       <h1>Recipe details</h1>
     </header>
   );
 }
+
+RecipeDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }),
+  }).isRequired,
+};
 
 export default RecipeDetails;
