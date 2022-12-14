09faebeaf1fdd3c606bda6e2f99515e976252f66
Mateus de Medeiros Ramos <mateusdmramos@gmail.com>
2022-12-14 16:23:35 -0300

    Requisito 35 realizado, feat: o botão de favoritos agora tem um icone de coração que muda de acordo com o status de favorito

--- src/components/FavoriteBtn.js
+++ src/components/FavoriteBtn.js
@@ -1,8 +1,27 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import recipesContext from '../context/recipesContext';
+import blackHeartIcon from '../images/blackHeartIcon.svg';
+import whiteHeartIcon from '../images/whiteHeartIcon.svg';
 
 function FavoriteBtn() {
   const { details } = useContext(recipesContext);
+  const [favoriteFood, setFavoriteFood] = useState(false);
+
+  useEffect(() => {
+    const url = window.location.href;
+    const favorites = JSON.parse(localStorage.getItem('favoriteRecipes'));
+    if (favorites !== null) {
+      if (url.includes('meals')) {
+        const validateFavorite = favorites
+          .some((favorite) => (favorite.id === details.idMeal));
+        setFavoriteFood(validateFavorite);
+      } else {
+        const validateFavorite = favorites
+          .some((favorite) => (favorite.id === details.idDrink));
+        setFavoriteFood(validateFavorite);
+      }
+    }
+  }, [details.idMeal, details.idDrink]);
 
   const saveFavoriteInLocalStorage = (favorite) => {
     const favorites = JSON.parse(localStorage.getItem('favoriteRecipes'));
@@ -11,6 +30,7 @@ function FavoriteBtn() {
     } else {
       localStorage.setItem('favoriteRecipes', JSON.stringify([...favorites, favorite]));
     }
+    setFavoriteFood(true);
   };
 
   const newFavoriteMeal = () => {
@@ -39,10 +59,27 @@ function FavoriteBtn() {
     saveFavoriteInLocalStorage(newRecipe);
   };
 
+  const validateFavoriteList = (url) => {
+    const favorites = JSON.parse(localStorage.getItem('favoriteRecipes'));
+    if (favorites !== null) {
+      if (url.includes('meals')) {
+        const validateFavorite = favorites
+          .some((favorite) => (favorite.id === details.idMeal));
+        return validateFavorite;
+      }
+      const validateFavorite = favorites
+        .some((favorite) => (favorite.id === details.idDrink));
+      return validateFavorite;
+    }
+    return false;
+  };
+
   const handleFavorite = () => {
-    if (window.location.href.includes('meals')) {
+    const url = window.location.href;
+    const isFavorite = validateFavoriteList(url);
+    if (url.includes('meals') && !isFavorite) {
       newFavoriteMeal();
-    } else {
+    } else if (url.includes('drinks') && !isFavorite) {
       newFavoriteDrink();
     }
   };
@@ -51,10 +88,13 @@ function FavoriteBtn() {
     <div>
       <button
         type="button"
-        data-testid="favorite-btn"
         onClick={ handleFavorite }
       >
-        Favoritar
+        <img
+          src={ favoriteFood ? blackHeartIcon : whiteHeartIcon }
+          alt="favorite icon"
+          data-testid="favorite-btn"
+        />
       </button>
     </div>
   );
