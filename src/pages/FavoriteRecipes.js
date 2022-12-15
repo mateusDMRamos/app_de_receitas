db83874f6ab207d1c89f865cc9ceb3bd9d3f07b3
Mateus de Medeiros Ramos <mateusdmramos@gmail.com>
2022-12-15 15:15:45 -0300

    feat: requisito 50 concluido, tela de favoritos agora mostra as receitas favoritadas

--- src/pages/FavoriteRecipes.js
+++ src/pages/FavoriteRecipes.js
@@ -2,10 +2,56 @@ import React from 'react';
 import Header from '../components/Header';
 
 function FavoriteRecipes() {
+  const [favoriteRecipes, setFavoriteRecipes] = useState([]);
+
+  useEffect(() => {
+    if (localStorage.getItem('doneRecipes')) {
+      const favRecipesLocalStorage = JSON.parse(localStorage.getItem('favoriteRecipes'));
+
+      setFavoriteRecipes(favRecipesLocalStorage);
+    }
+  }, []);
+
   return (
-    <header>
-      <Header title="Favorite Recipes" searchIcon={ false } />
-    </header>
+    <div>
+      <Header title="Done Recipes" searchIcon={ false } />
+      <button
+        data-testid="filter-by-all-btn"
+        type="button"
+      >
+        All
+      </button>
+      <button
+        data-testid="filter-by-meal-btn"
+        type="button"
+      >
+        Meals
+      </button>
+      <button
+        data-testid="filter-by-drink-btn"
+        type="button"
+      >
+        Drinks
+      </button>
+      { favoriteRecipes.length > 0
+        && favoriteRecipes.map((recipe, index) => (
+          <div key={ index }>
+            <img
+              src={ recipe.image }
+              alt={ recipe.name }
+              data-testid={ `${index}-horizontal-image` }
+            />
+            <p data-testid={ `${index}-horizontal-name` }>{ recipe.name }</p>
+            <p data-testid={ `${index}-horizontal-top-text` }>{recipe.category}</p>
+            <button
+              data-testid={ `${index}-horizontal-share-btn` }
+              type="button"
+            >
+              Compartilhar
+            </button>
+          </div>
+        ))}
+    </div>
   );
 }
 
